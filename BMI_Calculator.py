46973dce75f5b7f65ac7498b4c58e3d5a39b05f7
Amal K <12756149+1Amal@users.noreply.github.com>
2022-04-30 15:47:11 +1000

    Update BMI_Calculator.py
    Updated BMI calculator accuracy to two decimal places

--- BMI_Calculator.py
+++ BMI_Calculator.py
@@ -7,8 +7,9 @@ height = input("enter your height in M: ") # Code to capture height
 weight = input("enter your weight in kg: ") # Code to capture weight
 
 BMI=(int(weight))/(float(height) ** 2) # Code to calcualt BMI, BMI=weight/height^2
-int_BMI= int(BMI)
-print_BMI=str(int_BMI)
+# int_BMI= round(int(BMI), 4)
+rounded_BMI=round(BMI, 2) # Round the BMI to two decimal places
+print_BMI=str(rounded_BMI)
 print("Your BMI is: " + print_BMI)
 
 print("Meaning of BMI Ranges, Less than 18.5: Underweight, 18.5 to 24.9: Healthy weight, 25 to 29.9: Overweight but not obese") 
